e7138d25c0d4389f037706ba7ca526772d058a83
unknown <bitf19a003@pucit.edu.pk>
2022-12-06 17:56:15 +0500

    added put extra

--- app/src/main/java/com/example/intents/MainActivity.java
+++ app/src/main/java/com/example/intents/MainActivity.java
@@ -5,20 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.changeActivity);
+        editText = findViewById(R.id.text);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("view", editText.getText().toString());
                 startActivity(intent);
             }
         });
